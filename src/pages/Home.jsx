e3c75acf7e2d8f5e34f16518f3d5448fe3882551
Brian Cheptiony <cheptiony6@gmail.com>
2025-09-30 13:02:33 +0300

    fix vite

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Hero from "../components/Hero";
-import About from "../components/About";
-import Skills from "../components/Skills";
-import Projects from "../components/Projects";
-import Contact from "../components/Contact";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
