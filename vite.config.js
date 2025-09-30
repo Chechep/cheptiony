d605ef11e82e95c18a780311042164b78bb926bd
Brian Cheptiony <cheptiony6@gmail.com>
2025-09-30 13:57:15 +0300

    updates

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // <- add this
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
